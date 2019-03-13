be45bb33dbb3ea41a93c72bc59ef4785a08caf80
Michael A <hello@michaelamprimo.com>
2019-03-13 16:22:07 -0400

    ✨Create BaseMenuBar

---
+++ src/components/BaseMenuBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="menu-bar">
+    <slot/>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.menu-bar {
+  border-bottom: 1px solid#E8E8E8;
+  display: flex;
+  justify-content: center;
+  .menu-link {
+    color: #B3BDD1;
+    padding: 1rem 0;
+    margin: 0 1rem;
+    border: 2px solid transparent;
+    transition: all $transition-time;
+    &:hover,
+    &.router-link-active {
+      color: $secondary;
+      border-bottom: 2px solid $secondary;
+    }
+  }
+}
+</style>
