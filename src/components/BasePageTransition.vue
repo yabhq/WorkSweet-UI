e75138058c74245286b3b5c67671146994476934
Michael A <hello@michaelamprimo.com>
2019-03-14 11:10:48 -0400

    ✨Add Page transition

---
+++ src/components/BasePageTransition.vue
@@ -0,0 +1,14 @@
+<template>
+  <transition name="fade">
+    <slot/>
+  </transition>
+</template>
+
+<style lang="scss" scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .2s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
